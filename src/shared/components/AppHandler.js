ae85539cd6d122cfa6bb2fb5002f5ee3dbe8edd6
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2015-09-24 23:59:11 +0100

    add line break in AppHandler.js

--- src/shared/components/AppHandler.js
+++ src/shared/components/AppHandler.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default class AppHandler extends React.Component {
   render() {
-    return <div>Hello, { this.props.name }! This text was rendered by the { this.props.renderedBy }.</div>;
+    return <div>Hello, { this.props.name }!
+      This text was rendered by the { this.props.renderedBy }.</div>;
   }
 }
